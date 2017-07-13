047af8c6af283a09bed91d14e1cad8bdabe55393
Kislov Dmitry <kislov@easydan.com>
2017-07-13 11:31:59 +1000

    Help texts added

--- sciprod/models.py
+++ sciprod/models.py
@@ -1,8 +1,11 @@
+#coding: utf-8
+
 from django.db import models
 
 
 class WorkType(models.Model):
     type = models.CharField(max_length=200, default='')
+    abbr = models.CharField(max_legnth=2, default='')
 
     def __unicode__(self):
        return  self.type
@@ -29,12 +32,26 @@ class ComputationRules(models.Model):
 
 
 class ScientificWork(models.Model):
-    name = models.CharField(max_length=300, default='')
-    type = models.ForeignKey('WorkType', blank=True, null=True)
-    journal = models.ForeignKey('Journal', blank=True, null=True)
-    year = models.PositiveIntegerField(default=2017, blank=True)
-    pages = models.CharField(max_length=15, blank=True, default='')
-    attachment = models.FileField(uploadto='sciprod/%Y/%m/%d')
-    link = models.URLField(blank=True)
-
+    name = models.CharField(max_length=300, default='',
+                            verbose_name="название",
+                            help_text="название научной работы")
+    type = models.ForeignKey('WorkType', blank=True, null=True,
+                             verbose_name="тип работы",
+                             help_text="выберите тип научной работы")
+    journal = models.ForeignKey('Journal', blank=True, null=True,
+                                verbose_name="название журнала",
+                                help_text="выберите или добавьте недостающий журнал")
+    year = models.PositiveIntegerField(default=2017, blank=True,
+                                       vaerbose_name="год издания",
+                                       help_text="год опубликования работы")
+    pages = models.CharField(max_length=15, blank=True, default='',
+                             verbose_name="страницы",
+                             help_text="страницы через дефис, т.е. в формате: xx-yy ")
+    attachment = models.FileField(uploadto='sciprod/%Y/%m/%d',
+                                  verbose_name="Файл",
+                                  help_text="если файлов несколько, загрузите архив")
+    link = models.URLField(blank=True, verbose_name="ссылка",
+                           help_text="ссылка на Интернет ресурс")
+    user = models.ForeignKey(get_user_model(), null=True, blank=True,
+                             editable=False)
 
